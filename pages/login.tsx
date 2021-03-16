c171572974ac82015dd3c7d55345d64f346ef865
hyun <coderhyun476@gmail.com>
2021-03-16 21:25:47 +0900

    login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,17 +1,63 @@
-import React, { useEffect } from 'react'
+import { Button, Checkbox, Form, Input } from 'antd'
+import React, { useCallback, useEffect } from 'react'
+import styled from 'styled-components'
 import { auth } from '../lib/firebase'
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+`
+
+const FormContainer = styled(Form)`
+    width: 50vw;
+`
 
 const login = () => {
 
-    useEffect(() => {
-        // auth.signInWithEmailAndPassword('123@gmail.com', '123123')
+    const onFinish = useCallback((v) => {
+        auth.signInWithEmailAndPassword(v.email, v.password)
     }, [])
 
 
     return (
-        <div>
-            <h1>login</h1>
-        </div>
+        <Container>
+            <FormContainer
+                name="basic"
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    label="이메일"
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: '이메일을 입력해주세요',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="비밀번호"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: '비밀번호를 입력해주세요',
+                        },
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item >
+                    <Button type="primary" htmlType="submit">로그인</Button>
+                </Form.Item>
+            </FormContainer>
+        </Container>
     )
 }
 
